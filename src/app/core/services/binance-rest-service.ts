8b4e5f77800756bfc639988b557c464336cee249
IvanBuzhynski <buzhinin4@gmail.com>
2025-11-19 18:13:32 +0300

    Add depth and klines types and REST methods

--- src/app/core/services/binance-rest-service.ts
+++ src/app/core/services/binance-rest-service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, forkJoin, map, Observable } from 'rxjs';
 import { BinancePair, ExchangeInfo } from '../types/pair.interface';
+import { BinanceInterval, BinanceKline } from '../types/knile.interface';
+import { BinanceBid, BinanceDepth, BinanceLimit } from '../types/depth.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +18,60 @@ export class BinanceRestService {
 
   getStats(): Observable<BinancePair[]> {
     return this.http.get<BinancePair[]>(`${this.URL}/fapi/v1/ticker/24hr`);
+  }
+
+  getKlines(
+    symbol: string,
+    interval: BinanceInterval,
+    limit?: number,
+    startTime?: number,
+    endTime?: number
+  ): Observable<BinanceKline[]> {
+    let params = new HttpParams().set('symbol', symbol.toUpperCase()).set('interval', interval);
+
+    if (limit && limit >= 500 && limit <= 1500) params = params.set('limit', limit.toString());
+    if (startTime) params = params.set('startTime', startTime.toString());
+    if (endTime) params = params.set('endTime', endTime.toString());
+
+    return this.http.get<any[][]>(`${this.URL}/fapi/v1/klines`, { params }).pipe(
+      map((rawData) =>
+        rawData.map(
+          (kline) =>
+            ({
+              openTime: kline[0],
+              closeTime: kline[6],
+              open: kline[1],
+              high: kline[2],
+              low: kline[3],
+              close: kline[4],
+              volume: kline[5],
+              quoteAssetVolume: kline[7],
+            } as BinanceKline)
+        )
+      )
+    );
+  }
+
+  getDepth(symbol: string, limit: BinanceLimit = 500): Observable<BinanceDepth> {
+    const params = new HttpParams()
+      .set('symbol', symbol.toUpperCase())
+      .set('limit', limit.toString());
+
+    return this.http.get<any>(`${this.URL}/fapi/v1/depth`, { params }).pipe(
+      map(
+        (raw) =>
+          ({
+            lastUpdateId: raw.lastUpdateId,
+            transactionTime: raw.T,
+            bids: raw.bids.map(
+              ([price, quantity]: [string, string]) => ({ price, quantity } as BinanceBid)
+            ),
+            asks: raw.asks.map(
+              ([price, quantity]: [string, string]) => ({ price, quantity } as BinanceBid)
+            ),
+          } as BinanceDepth)
+      )
+    );
   }
 
   getPairsWithInfo(): Observable<BinancePair[]> {
---
+++ src/app/core/types/knile.interface.ts
@@ -0,0 +1,27 @@
+export interface BinanceKline {
+  openTime: number;
+  closeTime: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  quoteAssetVolume: string;
+}
+
+export type BinanceInterval =
+  | '1m'
+  | '3m'
+  | '5m'
+  | '15m'
+  | '30m'
+  | '1h'
+  | '2h'
+  | '4h'
+  | '6h'
+  | '8h'
+  | '12h'
+  | '1d'
+  | '3d'
+  | '1w'
+  | '1M';
