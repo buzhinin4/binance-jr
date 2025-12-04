0807d9e14f68cd3cf0f56f788bf710cff3c68745
IvanBuzhynski <buzhinin4@gmail.com>
2025-12-04 15:08:01 +0300

    Add to config files pairs feature store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,16 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { pairsFeature } from './store/pairs/pairs.reducer';
+import { PairsEffects } from './store/pairs/pairs.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +19,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideStore({
+      [pairsFeature.name]: pairsFeature.reducer,
+    }),
+    provideEffects([PairsEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
