7748020d1ba4664a2fb799c611de8ea9b222e3e1
IvanBuzhynski <buzhinin4@gmail.com>
2025-12-08 12:27:43 +0300

    Add agg trade and mark price types

---
+++ src/app/core/types/aggTrade.interface.ts
@@ -0,0 +1,5 @@
+export interface aggTrade {
+  price: number; // "p"
+  volume: number; // "q" in BTC
+  time: number; // "T"
+}
---
+++ src/app/core/types/markPrice.inteface.ts
@@ -0,0 +1,5 @@
+export interface markPrice {
+  markPrice: number; // "p"
+  indexPrice: number; // "i"
+  fundingRate: number; // "r"
+}
